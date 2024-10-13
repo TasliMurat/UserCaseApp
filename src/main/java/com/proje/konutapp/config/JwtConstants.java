11c612cf0d8361be9427d68133ef41cf047afc7c
Murat Taşlı <102462203+TasliMurat@users.noreply.github.com>
2024-10-14 00:12:34 +0300

    Update JwtConstants.java

--- src/main/java/com/proje/konutapp/config/JwtConstants.java
+++ src/main/java/com/proje/konutapp/config/JwtConstants.java
@@ -1,6 +1,6 @@
 package com.proje.konutapp.config;
 
 public class JwtConstants {
-    public  static final String JWT_SECRET = "njnfdjdkfnssndasndaskdlasfdkasldnfsvbnoıw";
+    public  static final String JWT_SECRET = "njnfdjdkfnssndasndaskdlasfdkaghfghfrghfghfghfg";
     public static final String JWT_HEADER = "Authorization";
 }
